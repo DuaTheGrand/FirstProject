c9083a14595dd76e2172eb3ae685e6cfb68cbf08
DuaTheGrand <76616620+DuaTheGrand@users.noreply.github.com>
2021-04-15 23:17:49 +0300

    Add files via upload

---
+++ Оценки.py.py
@@ -0,0 +1,11 @@
+mark = float(input("Enter your mark"))
+if mark > 0 and mark < 50:
+    print('2')
+elif mark > 50 and < 65:
+    print('3')
+elif mark > 65 and < 80:
+    print('4')
+elif mark >80 and < 100:
+    print('5')
+else:
+    print("Marks are between 0 and 100")
