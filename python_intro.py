ea2e2ab085b851caaf1db2928a816f78e1e683bc
DuaTheGrand <76616620+DuaTheGrand@users.noreply.github.com>
2021-04-15 22:49:04 +0300

    Add files via upload

--- python_intro.py
+++ python_intro.py
@@ -1,42 +1,9 @@
-# первый проект
-if 5 > 2:
-    print('It works, Ruslan, Well Done!')
-else:
-    print("It doesnt works, you're badass!")
-
-
-# второй проект
-name = 'Ruslan'
-if name == 'Ruslan':
-    print('Hey, Ruslan!')
-elif name == 'Denis':
-    print('Hey, Denis!')
-else:
-    print('Hey, Arseniy!')
-
-
-# третий проект
-def hi(name):
-    if name == 'Ruslan':
-        print('Hi, Ruslan!')
-    elif name == 'Denis':
-        print('Hi, Denis!')
+for grade in range (0,101,20):
+    if grade < 50:
+        print(2)
+    elif grade < 70:
+        print(3)
+    elif grade  < 80:
+        print(4)
     else:
-        print('Hi, Arseniy!')
-
-hi('Ruslan')
-
-
-# четвертый проект
-def hi(name):
-    print('Hi, ' + name + '!')
-
-men = ['Denis', 'Arseniy', 'Arslan', 'Artem', 'Kirill', 'Ruslanbek', 'Me']
-for name in men:
-    hi(name)
-    print('Next man')
-
-
-# пятый проект
-for I in range(1, 10):
-    print(I)
+        print(5)
